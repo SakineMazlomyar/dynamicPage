08b20bdcfc4e46921bddfbd588c182797781cce8
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-08 21:51:20 +0100

    addedNavigation

---
+++ views/navigation.php
@@ -0,0 +1,11 @@
+<?php
+return 
+"   <nav>
+        <ul>
+        <li><a href='index.php?page=skills'> My skills and background</a></li>
+        <li><a href='index.php?page=projects'> All projects I have done</a></li>
+        <li><a href='index.php?page=familj'> About my familj </a></li>
+        </ul>
+    </nav>
+"
+?>
