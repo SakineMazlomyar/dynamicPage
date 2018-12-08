05d2dfbbe8db9cbf294fec0ddad39778f833189b
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-08 21:52:00 +0100

    addedFooter

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<?php
+ return "
+        <footer>
+            <ul>
+                <li>contact</li>
+                <li>home</li>
+                <li>moreino</li>
+            </ul>
+        </footer>
+        "
+?>
