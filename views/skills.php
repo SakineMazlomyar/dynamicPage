9d72a0f62482009564d83a272d937f11a6f8f2f5
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-10 19:37:43 +0100

    fixedThePos

--- views/skills.php
+++ views/skills.php
@@ -1,4 +1,12 @@
 <?php
-return "<h1>Read about my skills</h1><br
-    <p>Read about my all format and training</p>"
+return "
+    <div style='position: relative; top: 5em;'>
+        <p>Read about my all format and training</p>
+        <ul>
+            <li>Read about my html and css projects</li>
+            <li>Read about my php projects</li>
+            <li>Read about my javascript and css projects</li>
+        <ul>
+    </div>
+    "
 ?>
